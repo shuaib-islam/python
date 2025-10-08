712fb6ac043c61c798604a21665cfceaf084524e
shuaib-islam <rashdanshabib@gmail.com>
2025-10-08 22:11:22 +0600

    polygen.py

---
+++ createcanvas.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to the turtle window.")
+
+turtle.done()
---
+++ polygon.py
@@ -0,0 +1,32 @@
+import turtle
+
+turtle.Screen().bgcolor("green")
+
+board = turtle.Turtle()
+
+# triangle
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+
+# square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+turtle.done()
