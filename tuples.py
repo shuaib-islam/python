a291d69b70fd7eb41cc9ad6a3748f7fbb2c65d5c
shuaib-islam <rashdanshabib@gmail.com>
2025-10-29 22:14:14 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,21 @@
+# empty tuple
+my_tuple =()
+
+#tuples with integer
+
+my_tuple =(1 , 2 , 3)
+
+#tuples with mixed data types
+
+my_tuple =("wyter", 1 , 2 , 3)
+
+#nested tuple
+
+my_tuple =("myter", [1 , 2, 3], (5 , 6 , 7))
+
+#Accessing tuples with mixed indexes
+
+my_tuple =("q", "r", "s", "t", "u")
+print(my_tuple[1])
+print(my_tuple[3])
+
