fffa04403e48f3b9b6de017885b6e94e6f85bc77
shuaib-islam <rashdanshabib@gmail.com>
2025-10-30 21:52:04 +0600

    patterns

---
+++ patterns.py
@@ -0,0 +1,15 @@
+for i in range(1,6):
+    for j in range(i):
+        print("*", end="  ")
+    print("\n")
+    
+    
+#inverted star
+    
+for i in range(6, 1, -1):
+    
+    for j in range(i, 1, -1):
+        print("*", end="  ")
+    print("\n")
+    
+    
