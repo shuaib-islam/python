c2779544e233dfc3e19a3757d348e8df9405a229
shuaib-islam <rashdanshabib@gmail.com>
2025-11-06 22:14:53 +0600

    Phonebook2

--- phonebook.py
+++ phonebook.py
@@ -37,3 +37,58 @@ def intial_phonebook():
             phone_book.append(temp)
         print(phone_book)
         return phone_book
+    
+def menu():
+        print("1. Add a new contact")
+        print("2. Remove an existing contact")
+        print("3. Delete all contacts")
+        print("4. Search for a contact")
+        print("5. Display all contacts")
+        print("6. Exit phonebook")
+        
+        choice = int(input("Please enter your choice"))
+        
+        return choice
+    
+def add_contact(pb):
+
+        dip = []
+        for i in range(len(pb[0])):
+                if i == 0:
+                        dip.append(str(input("Enter name:  ")))
+                if i == 1:
+                        dip.append(int(input("Enter number: ")))
+                if i == 2:
+                        dip.append(str(input("Enter e-mail adress:  ")))
+                if i == 3:
+                        dip.append(str(input("Enter date of birth(dd/mm/yy): ")))
+                if i == 4:
+                        dip.append(str(input("Enter category(Family/Freinds/Work/Others): ")))
+                        
+        pb.append(dip)
+        return pb
+        
+def remove_existing(pb):
+
+        query = str(input("Please enter the name of the contact you wish to remove"))
+        
+        temp = 0
+        
+        for i in range(len(pb)):
+            
+                if query == pb[i][0]:
+                        temp += 1
+                        
+                        print(pb.pop(i))
+                        print("This query has now been removed")
+                        
+                        
+        if temp == 0:
+        
+        
+                        print("Sorry, you have entered an invalid query / Pls recheck and try again later")
+                        
+def delete_all(pb):
+    
+        return pb.clear()
+    
