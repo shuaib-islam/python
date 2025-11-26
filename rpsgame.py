ff2da9f003e4f2295be00c66b81a27f16d3b186f
shuaib-islam <rashdanshabib@gmail.com>
2025-11-26 22:10:58 +0600

    rock your a paper

---
+++ rpsgame.py
@@ -0,0 +1,14 @@
+import random
+import pygame
+
+class Button():
+    def __init__(self, x, y, pos, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.pos = pos
+        
+    def clicked(self, pos):
+        self.pos = pygame.mouse.
+             
